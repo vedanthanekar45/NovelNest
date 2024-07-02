58b55aa9038e9ca75d5eb82c08bb5e1562ada1c9
vedanthanekar45 <vedanthanekar45@gmail.com>
2024-07-02 14:04:12 +0530

    Homepage changes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Navbar from "./components/Navbar"
 import './App.css'
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <nav className="flex-1">
+        <div className="logo">
+          <a>
+            <h2>NovelNest</h2>
+          </a>
+        </div>
+      </nav>
+    </div>
   );
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="flex-1">
+                <div className="logo">
+                    <a><h2>NovelNest</h2></a>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
