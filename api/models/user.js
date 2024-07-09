c6fc8b2cbf556dc0af30ea452493f11aacbfffdc
vedanthanekar45 <vedanthanekar45@gmail.com>
2024-07-09 15:06:53 +0530

    Fixed Authentication problems

--- api/models/user.js
+++ api/models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: String,
-    username: String,
-    password: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
 })
 
 const userModel = mongoose.model("users", userSchema);
-module.exports = userModel
+module.exports = userModel;
